b94ad0d0ea114712077154371844e8a5ddde1ed2
Michael Gauthier <mike@silverorange.com>
2014-04-17 19:21:51 -0300

    Add more standard media encodings that match up with BoTR.

--- sql/fixtures/BuildingBlockMediaEncoding.sql
+++ sql/fixtures/BuildingBlockMediaEncoding.sql
@@ -8,7 +8,55 @@ insert into MediaEncoding (
 ) values (
 	(select id from MediaSet where shortname = 'block'),
 	(select id from MediaType where mime_type = 'video/mp4'),
-	'720p',
+	'1920-wide (1080p)',
+	'1920',
+	true,
+	1920
+);
+
+insert into MediaEncoding (
+	media_set,
+	default_type,
+	title,
+	shortname,
+	default_encoding,
+	width
+) values (
+	(select id from MediaSet where shortname = 'block'),
+	(select id from MediaType where mime_type = 'video/mp4'),
+	'1280-wide (720p)',
+	'1280',
+	true,
+	1280
+);
+
+insert into MediaEncoding (
+	media_set,
+	default_type,
+	title,
+	shortname,
+	default_encoding,
+	width
+) values (
+	(select id from MediaSet where shortname = 'block'),
+	(select id from MediaType where mime_type = 'video/mp4'),
+	'1080-wide',
+	'1080',
+	true,
+	1080
+);
+
+insert into MediaEncoding (
+	media_set,
+	default_type,
+	title,
+	shortname,
+	default_encoding,
+	width
+) values (
+	(select id from MediaSet where shortname = 'block'),
+	(select id from MediaType where mime_type = 'video/mp4'),
+	'720-wide',
 	'720',
 	true,
 	720
@@ -24,7 +72,7 @@ insert into MediaEncoding (
 ) values (
 	(select id from MediaSet where shortname = 'block'),
 	(select id from MediaType where mime_type = 'video/mp4'),
-	'480p',
+	'480-wide',
 	'480',
 	true,
 	480
@@ -40,8 +88,8 @@ insert into MediaEncoding (
 ) values (
 	(select id from MediaSet where shortname = 'block'),
 	(select id from MediaType where mime_type = 'video/mp4'),
-	'360p',
-	'360',
+	'320-wide (QVGA)',
+	'320',
 	true,
-	360
+	320
 );
